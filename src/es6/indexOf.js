acea1e06976152c84d5ab3e4be9fea028b1750c6
Volta <giovoltadesign@gmail.com>
2021-01-30 17:53:23 -0300

    update indexOf()

--- src/es6/indexOf.js
+++ src/es6/indexOf.js
@@ -21,7 +21,7 @@ la búsqueda se realizará por todo el array.
 let str = 'Winter is coming';
 let position = str.indexOf('coming');
 
-console.log(position);
+console.log(position);//10
 
 // El siguiente ejemplo usa indexof() para localizar valores en un array 
 
@@ -63,3 +63,94 @@ updateVegetablesCollection(veggies, 'espinaca');
 // La nueva colección de verduras es : patata, tomate, chiles, pimientoverde, espinaca
 updateVegetablesCollection(veggies, 'espinaca');
 // La espinaca ya existe en la colección de verduras.
+
+
+// otros ejemplos
+
+let str = 'Winter is coming';
+let position = str.indexOf('coming');
+
+console.log(position);//10
+
+// El método indexOf distingue entre mayúsculas y minúsculas. Por ejemplo, la siguiente expresión devuelve -1.
+
+let str = 'Winter is coming';
+let position = str.indexOf('winter');
+
+console.log(position);
+
+// no está presente en la Cadena. Entonces volverá a la posición -1.
+
+
+const str = 'Winter is coming';
+let count = 0;
+let position = str.indexOf('i');
+
+while (position !== -1) {
+  count++;
+  position = str.indexOf('i', position + 1);
+}
+
+console.log(count);//3 veces i dentro de str
+
+// Use indexOf () para contar las apariciones de una letra en 
+// String Cuentemos el número de apariciones de la letra b en la Cadena.
+// recuerda que este method es case sensitive
+
+const str = 'Millie Bobby Brown';
+let count = 0;
+let position = str.indexOf('b');
+
+while (position !== -1) {
+  count++;
+  position = str.indexOf('b', position + 1);
+}
+
+console.log(count);//2
+
+
+const str = 'Millie Bobby Brown';
+if (str.toLowerCase().indexOf("millie bobby brown") === -1) { 
+  console.log('Match Not found');
+}
+else {
+  console.log('Match found')
+}
+// Match found
+
+// Javascript includes() vs. indexOf()
+/* La diferencia entre el método includes () y el método indexOf () 
+es que el método Javascript string includes () determina si una 
+cadena puede encontrarse dentro de otra cadena, devolviendo verdadero 
+o falso según corresponda. El método indexOf () devuelve un index dentro 
+del objeto String que llama de la primera aparición de un valor especificado, 
+comenzando la búsqueda en fromIndex ().*/
+
+// String indexOf() function with regex
+
+// Veamos el ejemplo anterior, y si encontramos la coincidencia, imprimirá la coincidencia encontrada, de lo contrario no.
+
+const str = 'Millie Bobby Brown';
+if (!/millie/i.test(str)) {
+  console.log('Match Not found');
+}
+else {
+  console.log('Match found')
+}
+// output Match Not found
+
+// Difference between String indexOf() and search()
+
+// Javascript search() function takes the regular expression, which allows you to match 
+// against more sophisticated patters, case-insensitive strings, etc.
+
+// An indexOf simply matches the literal String. However, indexOf also allows you to specify 
+// a beginning index.
+
+/*Conclusion
+El método Javascript String indexOf () devuelve la posición de 
+la primera aparición de un valor especificado en una cadena o una
+matriz. Si desea encontrar la posición de un valor especificado en String, 
+utilice el método Javascript String indexOf (). Si desea encontrar un índice del 
+elemento en particular en la matriz, utilice el método indexOf () de la matriz de Javascript.*/
+
