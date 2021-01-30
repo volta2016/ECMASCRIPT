903791c59dcdadc49d64e03e070f6fd8fe1fe0ca
Volta <giovoltadesign@gmail.com>
2021-01-30 20:19:48 -0300

    reserved words

---
+++ src/ language-characteristics/reserved-words.js
@@ -0,0 +1,16 @@
+A: abstract
+B: boolean, break, byte
+C: case, catch, char, class, const, continue
+D: debugger, default, delete, do, double
+E: else, enum, export, extends
+F: false, final, finally, float, for, function
+G: goto
+I: if, implements, import, in, instanceof, int, interface
+L: let, long
+N: native, new, null
+P: package, private, protected, public
+R: return
+S: short, static, super, switch, synchronized
+T: this, throw, throws, transient, true, try, typeof
+V: var, volatile, void
+W: while, with
