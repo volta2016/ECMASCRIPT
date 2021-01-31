3b9c512ac6753be18a0fb7f46ea420f31cbe62b3
Volta <giovoltadesign@gmail.com>
2021-01-31 18:31:16 -0300

    code execution - type of data js

---
+++ src/ language-characteristics/ type-of-data.js
@@ -0,0 +1,20 @@
+// Tipos de datos en javascript
+
+// Primitivos: Se accede directamente al valor
+string:
+console.log(typeof ('nuevo texto'));
+number:
+console.log(typeof (2));
+boolean:
+console.log(typeof (false));
+console.log(typeof (true));
+null:
+console.log(typeof (null));//object esto viene así desde el core del lenguaje
+NaN:
+console.log(typeof (NaN));
+
+// Object o Compuestos:
+object = {}
+array = []
+function () { }
+Class {}
