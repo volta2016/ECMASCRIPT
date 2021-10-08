478c33a1e7617e55aae7586ae3eda82a83fbc459
Volta <giovoltadesign@gmail.com>
2021-10-08 10:46:14 -0300

    [js]  difference-forEach-map - sumary

--- src/ language-characteristics/hoisting/index.js
+++ src/ language-characteristics/hoisting/index.js
@@ -1,195 +1,72 @@
-/** ::"Hoisting":: es un concepto fundamental del lenguaje JavaScript debido a que te ayuda a entender cómo tu código 
-es ejecutado y por ello, es uno de los temas que se preguntan con frecuencia en las entrevistas (en serio) .
-
-Aunque el concepto es muy sencillo, la mayoría de las veces se malinterpreta. En este artículo vas a aprender 
-qué es hoisting con la ayuda de algunos ejemplos.
-
-Lo que vas a aprender
-Después de leer este artículo vas entender:
-
-El contexto de ejecución y sus fases.
-Cómo funciona el hoisting con variables (var) y funciones (function)
-Casos especiales de hoisting (const y let).
-Qué es la Temporal Dead Zone.**/
-
-var source = "https://github.com/volta2016";
-console.log(source);
-
-function printSource() {
-	console.log(`"from printSource:" ${source}`);
-}
-
-printSource();
-
-//salida:
-/*https://github.com/volta2016
-desde print: https://github.com/volta2016
-
-El código se ejecuta línea por línea.
-
-Echemos un vistazo a lo que sucede:
-
-Se crea la variable source y se le asigna un valor a https://github.com/volta2016 .
-Se encuentra la línea de código console.log y el valor de source se imprime en la consola: https://github.com/volta2016.
-Del mismo modo, la siguiente línea es la definición de la función printSource. Seguido de la llamada real a la función printSource(). 
-Esto resulta en las declaraciones dentro de la función que se ejecuta y la cadena desde print: https://github.com/volta2016se 
-imprime en la consola.
-Pero, ¿es realmente tan sencilla la ejecución de código en JavaScript?
-
-Echa un vistazo a esta variación del mismo ejemplo:
-
-*/
-
-console.log(source);
-printSource();
-
-function printSource() {
-	// definición de la función
-	console.log(`"from printSource:" ${source}`);
-}
-
-var source = "https://github.com/volta2016";
-printSource();
-
-/*Resultado:
-
-undefined
-desde print: undefined
-desde print: https://github.com/volta2016
-
-Hoisting en JavaScript te permite acceder a funciones y variables antes de que hayan sido creadas.
-
-::Contexto de Ejecución:: ⏰
-
-El contexto de ejecución es el entorno que prepara el motor de JavaScript para ejecutar el código que escribimos.
-En pocas palabras, es la información necesaria para ejecutar el código.
-El contexto de ejecución se crea en dos fases:
-
-
-1 - ::Fase de creación:: 🤞
-
-
-- El código es escaneado/parseado para las variables y funciones.
-- Se reserva espacio para las variables en memoria.
-- Las definiciones de las funciones se cargan en la memoria.
-
-
-
-2- ::Fase de ejecución:: ⏏️
-
-- El código se ejecuta línea por línea con la información de la fase de creación.
-- Se asignan valores a las variables.
-- Se ejecutan las funciones.
-
-
-vamos a ver otro ejemplo de contexto de ejcución
-*/
-
-source = 5
-console.log(source)
-printFinal()
-var printFinal = function print() {
-	console.log(`desde la funcion: ${source}`)
-}
-
-var source;
 
 /*
-* El valor 5 se asigna a source.
-* 5 se imprime en la consola.
-* Se llama a printFnExp. Sin embargo, arroja un error - Uncaught TypeError: printFnExp no es una función.
+function name(name) {
+	return (this.name = `${name} :)`);
+}
 
-Para solucionar este error, vea los cambios en el código que aparecen a continuación:
+var user = new name("Kyo"); //estoy guardando una isntancia de mi funcion por que la estoy usando de manera
+//de clase
+console.log(user.name);
+
+*/
+
+/*
+const myFunction = (name) => {
+	console.log(this.name = `${name} :)`;
+};
+
+*/
+
+/*
+function time() {
+  
+  this.seg = 0;
+  setInterval( () => {
+    this.seg++
+    console.log(this.seg)
+  }, 1000)
+}
+
+new time
+
 
 */
 
 
-source = 5
-console.log(source)
-var printFinal = function print() {
-	console.log(`desde la funcion: ${source}`)
-}
-printFinal()
-
-var source;
 
 
-/*Salida:
 
-5
-desde la función: 5
+//const sumarDos = num1 => num1
+//console.log(sumarDos(10))
 
+  /*  let option = (prompt('ingrese numero'))
 
-Casos Excepcionales
-- var se inicializa con undefined
-- en el caso de let y const el valor se inicializa solo durante la fase de ejecución
-
-
-*/
-
-console.log(source);
-print();
-function print() {
-  console.log("desde print: " + source);
-}
-let source = "@volta2016";
-
-//Salida:
-
-//Uncaught ReferenceError: source is not defined
-
-//ahora vamos aver el uso correcto de let:
+    let primo = true 
+    // debugger
+     for(let i = 2 ;i< option;i++){
+         let resultado =parseInt(option/i)
+         if( option / i ==resultado ) {
+              primo =false
+              } else if(option  > 1 && option /1 == option && option/option == 1)  {
+                primo = true
+     }
+     }
 
 
-let source = "@volta2016";
-
-console.log(source);
-
-function print() {
-	console.log("desde print: " + source);
-}
-print();
+     if ( primo == true ) {
+        console.log(el numero ${option}  es primo)
+     }  else  {
+        console.log(el numero ${option} no  es primo)
+     }*/
 
 
-//Durante la fase de ejecución, si no se proporciona ningún valor junto con la declaración, el valor se considera undefined.
+const numerosTwo = [1, 2, 3, 4, 5];
 
-//Veamos el siguiente ejemplo
-
-let source; // declaración
-console.log(source);
-source = "@volta2016"; // inicialización
-function print() {
-  console.log("desde print: " + source);
-}
-print();
+const mutiplicar = numerosTwo.forEach(x => x * 5);
+console.log(mutiplicar);
 
 
-//Salida:
-
-//undefined
-//from print: @volta2016
 
 
-/* Ejemplo con const 
-
-*/
-
-const source;
-console.log(source)
 
 
-//Salida:
-
-//Uncaught SyntaxError: Missing initializer in const declaration
-
-//Una const indica un valor constante. Este valor no puede ser cambiado durante la ejecución del código. Por lo tanto, tiene sentido que requiera un valor inicializador en el momento de la declaración.
-//Uso correcto de const:
-
-const source = "@volta2016";
-console.log(source);
-
-
-/* resumen:
-
-"Hoisting es cuando las funciones y las variables se almacenan en 
-memoria para un contexto de ejecución antes de ejecutar nuestro código." */
--- src/es6/lenguage-javascript/difference-forEach-map/difference-forEach-map.js
+++ src/es6/lenguage-javascript/difference-forEach-map/difference-forEach-map.js
@@ -2,12 +2,12 @@
 - ambas forman part de las funciones que podemos aplicarle a un array
 - las 2 cumplen con el proposito de iterar un arreglo nos permite hacer operaciones
 por cada uno de los elementos que permite nuestro array
-¿cual es la diferencia entre ellos la respuesta radica en que si vamos a utlizar 
+¿cual es la diferencia entre ellos ? la respuesta radica en que si vamos a utlizar 
 el resultado que entrega mas delante o no?
 */
 
 const numeros = [1, 2, 3, 4, 5];
-
+//ambos iteran cada elemento
 numeros.forEach((x) => {
 	console.log("forEach", x);
 });
@@ -15,3 +15,16 @@ numeros.forEach((x) => {
 numeros.map((x) => {
 	console.log("map", x);
 });
+
+//ejemplo 2:
+const numerosTwo = [1, 2, 3, 4, 5];
+
+//forEach si lo que necesitamos es recorrer nuestro arreglo y hacer operaciones temporales, pero que no se regresen que no podamos
+//almacenarlas para posterior utlizarlas lo unico que vamos a utlizar es forEach > pensemos en el escenario que es como for tradicional
+const mutiplicar = numerosTwo.forEach((x) => x * 5);
+console.log(mutiplicar); //forEach no permite almacenar el valor de la operaciones
+
+//el escenario de map es regresar un arreglo completamente nuevo > con las operaciones aplicadas
+const mutiplicar2 = numerosTwo.map((x) => x * 5);
+console.log(mutiplicar2); //podemos ver que si me esta devolviendo un nuevo array aplicando la operación [5, 10, 15, 20, 25];
+//
