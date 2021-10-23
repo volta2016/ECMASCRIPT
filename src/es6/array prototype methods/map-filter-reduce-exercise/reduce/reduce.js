3e1e90b723b37aa4743a1caa4b8314b8c93e0885
volta2016 <giovoltadesign@gmail.com>
2021-10-22 22:03:59 -0300

    [ts] function callback base

--- src/es6/array prototype methods/map-filter-reduce-exercise/reduce/reduce.js
+++ src/es6/array prototype methods/map-filter-reduce-exercise/reduce/reduce.js
@@ -1,10 +1,16 @@
 /*Reduce nos va servir para reducir todos los valores de ese arreglo
-a un unico valor que puede ser que puede ser un numero, un objeto, un array */
+a un unico valor que puede ser que puede ser un numero, un objeto, un array 
+
+El valor devuelto de la función reductora se asigna al acumulador, cuyo valor se recuerda en cada iteración de la matriz y, en última instancia, se convierte en el valor final, único y resultante.
+
+
+
+*/
 
 const edadesPersonas = [5, 7, 22, 30, 44];
 
-const totalEdades = edadesPersonas.reduce((acumulator, element) => {
-	return (acumulator += element);
+const totalEdades = edadesPersonas.reduce((acumulator, currentValue) => {
+	return (acumulator += currentValue);
 }, 0);
 
 console.log(totalEdades, "Suma de edades");
