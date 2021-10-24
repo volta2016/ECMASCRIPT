eb0dd5324005bd9cbf762d5eeb1f51bf4617059d
volta2016 <giovoltadesign@gmail.com>
2021-10-24 13:47:04 -0300

    [js] find

--- src/es6/array prototype methods/Javascript (Array methods)/example-methods/find.js
+++ src/es6/array prototype methods/Javascript (Array methods)/example-methods/find.js
@@ -1,3 +1,16 @@
 /*
-
+::find::
 El método find() devuelve el valor del primer elemento del array que cumple la función de prueba proporcionada.*/
+
+const users = [
+  { id: 1, name: "John", age: 33 },
+  { id: 2, name: "Kyo", age: 22 },
+  { id: 3, name: "Ameley", age: 27 },
+];
+
+const findKyo = users.find((user) => user.id === 2);
+console.log(findKyo);
+
+//utilizando destructuring
+const { age, id } = users.find((user) => user.id === 2);
+console.log(age, id);
