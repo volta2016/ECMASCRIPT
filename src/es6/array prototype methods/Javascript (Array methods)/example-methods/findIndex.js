b15dc62c0f9dc02d659e9d1ca738c0c4af6320bd
volta2016 <giovoltadesign@gmail.com>
2021-10-24 16:25:03 -0300

    [js] findIndex slice

--- src/es6/array prototype methods/Javascript (Array methods)/example-methods/findIndex.js
+++ src/es6/array prototype methods/Javascript (Array methods)/example-methods/findIndex.js
@@ -1,6 +1,5 @@
 /*
-
-
+::findIndex()::
 El método findIndex() devuelve el índice del primer elemento de un array que cumpla con la función de prueba proporcionada. En caso contrario devuelve -1.
 */
 
---
+++ src/es6/array prototype methods/Javascript (Array methods)/example-methods/slice.js
@@ -0,0 +1,11 @@
+/*
+
+El método slice() devuelve una copia de una parte del array dentro de un nuevo array empezando por inicio hasta fin (fin no incluido). El array original no se modificará.
+*/
+
+const animals = ["Cat", "Dog", "Serpents", "Zebra"];
+//             0     [1      2]        3
+
+const newAnimals = animals.slice(1, 3);
+
+console.log(newAnimals);
