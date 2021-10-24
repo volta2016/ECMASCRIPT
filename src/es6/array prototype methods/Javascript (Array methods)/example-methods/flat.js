679939501f0d0bcd61c78ea2c0c198b9b4978949
volta2016 <giovoltadesign@gmail.com>
2021-10-24 17:57:55 -0300

    [js] flat

--- src/es6/array prototype methods/Javascript (Array methods)/example-methods/flat.js
+++ src/es6/array prototype methods/Javascript (Array methods)/example-methods/flat.js
@@ -3,3 +3,17 @@ Experimental: Esta es una tecnología experimental
 El método flat() crea una nueva matriz con todos los elementos de sub-array concatenados recursivamente hasta la profundidad especificada.*/
 
 //Aplanar matrices anidadas #03:
+
+const arrayNumeros = [
+  [0, 1],
+  [2, 3],
+  [4, 5],
+];
+
+const flatSingle = arrayNumeros.flat();
+console.log(flatSingle); //[ 0, 1, 2, 3, 4, 5 ]
+
+const arrayNumerosDos = [1, 2, [3, 4, [5, 6]]];
+
+const arrayPlano = arrayNumerosDos.flat(2);
+console.log(arrayPlano); //nueva matriz con todos los elementos [ 1, 2, 3, 4, 5, 6 ]
