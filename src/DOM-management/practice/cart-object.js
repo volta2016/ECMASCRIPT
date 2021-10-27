b578212870983ed89c0fddd874b8065090fe501a
volta2016 <giovoltadesign@gmail.com>
2021-10-27 19:18:45 -0300

    [js] pacticebase

---
+++ src/DOM-management/practice/cart-object.js
@@ -0,0 +1,9 @@
+// alert("hola ok");
+const cart = document.querySelector("#id"); //cart ul
+const templatecheck = document.querySelector("#template");
+const footer = document.querySelector("#foooter");
+const footer_template = document.querySelector("#templateFooter");
+
+const fragment = document.createDocumentFragment();
+
+let cartArray = [];
