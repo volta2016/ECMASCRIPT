bd2b6277c21c5d50688bb0c2a3a0a2a6ec77c3c9
volta2016 <giovoltadesign@gmail.com>
2021-11-07 13:01:16 -0300

    [js] logic fragment clone

--- src/DOM-management/practice/cart-object.js
+++ src/DOM-management/practice/cart-object.js
@@ -15,56 +15,53 @@ const fragment = document.createDocumentFragment(); //node vacío NO pertenece a
 let cartArray = [];
 
 //delegacion de eventos > accediendo al boton agregar de la lista de productos
+// allButtons.forEach((btn) => btn.addEventListener("click", addCart));
+
 document.addEventListener("click", (e) => {
-  // console.log(e);
-  // console.log(e.target.dataset.fruit);
-  // console.log(e.target.matches(".card button"));
-  // if (e.target.matches(".card button")) {
-  //   addCart(e);
-  // }
-  // //accediendo al boton de agregar del template dle check total de los productos
-  // if (e.target.matches(".list-group-item .btn-succes")) {
-  //   btnIcrease(e);
-  // }
-  // if (e.target.matches(".list-group-item .btn-danger")) {
-  //   btnDecrease(e);
-  // }
+  if (e.target.matches(".card .btn-primary")) {
+    //pasa si nos da true
+    addCart(e);
+  }
 });
 
 const addCart = (e) => {
-  //console.log(e.target.dataset); //primero seleccionamos la fruta construimos nuestro producto que es un objeto
+  //console.log(e.target.dataset); //primero seleccionamos la fruta construimos nuestro producto que es un objeto por eso es que necesito e.target.dataset
   const product = {
     title: e.target.dataset.fruit,
     id: e.target.dataset.fruit,
     count: 1,
     price: parseInt(e.target.dataset.price), //lo pasamos a type number
   };
-
+  console.log(product);
   //buscamos el index
   const index = cartArray.findIndex((item) => item.id === product.id); //si es igual nos devuelve el index
   //console.log(index); //-1 por que el array esta vacío -1 es que no existe el producto
 
   //si no existe el index es -1 epujamos el nuevo elemento solo una vez agrego el elemento. Si el cliente vuelve agregar el mismo producto "NO" voy hacer push
+
   if (index === -1) {
     cartArray.push(product);
   } else {
-    //caso de que exista lo contrario aumentamos su count
-    cartArray[index].count++;
+    //caso contrario  que existe aumentamos su count
+    cartArray[index].count++; //accedemos al objeto en su indice específico
+    // cartArray[index].price = cartArray[index].count * product.price;
   }
   console.log(cartArray);
 
-  printCart(cartArray);
+  printCart();
 };
 
-const printCart = (array) => {
+const printCart = () => {
   cart.textContent = "";
-  array.forEach((item) => {
-    const clone = template.content.firstElementChild.cloneNode(true); //aplico la clonación del template
-    clone.querySelector(".lead").textContent = item.title; //al template modifico su title
+  cartArray.forEach((item) => {
+    const clone = template.content.cloneNode(true); //aplico la clonación del template
+    clone.querySelector(".text-white .lead").textContent = item.title; //al template modifico su title
     clone.querySelector(".badge").textContent = item.count; //empujo la cantidad
+    clone.querySelector("div .lead span").textContent = item.price * item.count;
+
+    clone.querySelector(".btn-danger").dataset.id = item.id;
+    clone.querySelector(".btn-success").dataset.id = item.id;
     fragment.appendChild(clone); //todo lo agrego al fragment para evitar el reflow
   });
   cart.appendChild(fragment);
 };
-
-allButtons.forEach((btn) => btn.addEventListener("click", addCart));
