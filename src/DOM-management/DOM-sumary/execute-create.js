82eb8d3b5238b6ce581caf2234f816df666ed830
volta2016 <giovoltadesign@gmail.com>
2023-01-09 16:05:14 -0300

    [js] execute-create arrow function

--- src/DOM-management/DOM-sumary/execute-create.js
+++ src/DOM-management/DOM-sumary/execute-create.js
@@ -20,4 +20,27 @@ button.addEventListener("click", function () {
   alert("clicked");
 });
 
+//the same
+// button.addEventListener("click",  () => {
+//   alert("clicked");
+// });
+
 document.body.append(button);
+
+//arrow function
+
+const showText = () => {
+  return "show text";
+};
+
+console.log(showText());
+//can return all this in one line
+
+const showText2 = () => "show text"; //there is implicit return
+console.log(showText2());
+
+const showText1 = () => "Hola Mundo";
+const showNumber = () => 23.3;
+const showBoolean = () => true;
+const showArray = () => [1, 2, 3];
+const showObject = () => ({ name: "Kyo" }); //by placing it in "()"" this is the object that will be returned.
