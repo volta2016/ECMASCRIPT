f6136520f690f84a9f2c12886a8bdc9f4842532a
volta2016 <giovoltadesign@gmail.com>
2023-08-22 16:30:40 -0400

    docs: :memo: single

---
+++ src/es6/async-await/single/single.js
@@ -0,0 +1,11 @@
+console.log("Hola mundo");
+
+const FIVE_SECONDS_IN_MS = 5000;
+
+function sayHej() {
+  console.log("Hej världen");
+}
+
+setTimeout(sayHej, FIVE_SECONDS_IN_MS);
+
+console.log("Hola world");
