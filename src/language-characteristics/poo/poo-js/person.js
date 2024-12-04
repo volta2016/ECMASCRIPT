3d1f29cae809668d80f8a74148e0dcf4400a4f80
volta2016 <giovoltadesign@gmail.com>
2024-12-04 13:05:25 -0300

    feat: :memo: inheritance in JS, update person.js

--- src/language-characteristics/poo/poo-js/person.js
+++ src/language-characteristics/poo/poo-js/person.js
@@ -4,15 +4,61 @@ class Person {
     this.countryOfBirth = countryOfBirth;
   }
 
-  grettings() {
+  greetings() {
+    // Cambiado de grettings a greetings
     console.log(
-      `Hello my name is ${this.name} and I live in no${this.countryOfBirth}`
+      `Hello, my name is ${this.name} and I live in ${this.countryOfBirth}.`
     );
   }
 }
 
-let objPerson = new Person('Kyo', 'Japon');
-objPerson.grettings();
+// let objPerson = new Person('Kyo', 'Japon');
+// objPerson.greetings();
 
-let objPerson2 = new Person('Lonel', 'Argentina');
-objPerson.grettings();
+// let objPerson2 = new Person('Leonel', 'Argentina');
+// objPerson2.greetings();
+
+class Employee extends Person {
+  constructor(name, countryOfBirth, company, profession, yearsOfWork) {
+    super(name, countryOfBirth);
+    this.company = company;
+    this.profession = profession;
+    this.yearsOfWork = yearsOfWork;
+  }
+
+  showWork() {
+    console.log(
+      `I work at ${this.company}. I'm a ${this.profession}, and I have ${this.yearsOfWork} years of experience.`
+    );
+  }
+}
+
+class Student extends Person {
+  constructor(name, countryOfBirth, university, semester) {
+    super(name, countryOfBirth);
+    this.university = university;
+    this.semester = semester;
+  }
+
+  showStudent() {
+    console.log(
+      `I study at ${this.university}. I'm in the ${this.semester} semester.`
+    );
+  }
+}
+
+let employee = new Employee(
+  'Kyo',
+  'Japan',
+  'Bank 2000',
+  'Software Developer',
+  8
+);
+
+employee.greetings();
+employee.showWork();
+
+let objStudent = new Student('Felipe', 'México', 'Harvard', 'fifth');
+
+objStudent.greetings();
+objStudent.showStudent();
